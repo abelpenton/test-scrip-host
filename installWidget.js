b6193bab470dc3a5d2044c86d0bacc7ab441d6be
apenton <abelp@protectgroup.com>
2024-01-22 16:39:49 -0300

    add installation script widget

---
+++ installWidget.js
@@ -0,0 +1,27 @@
+(function (P, R, O, t, E, C, T) {
+    P[t] = P[t] || function () { (P[t].q = P[t].q || []).push(arguments) };
+    C = R.createElement(O), T = R.getElementsByTagName(O)[0];
+    C.id = t; C.src = E;
+    T.parentNode.insertBefore(C, T);
+
+    P._pgr('init', {
+        environment: 'test',
+        debug: true,
+        vendorCode: 'ven_local_6ba3e093c67c428b90670440441469a2',
+        currencyCode: 'USD',
+        languageCode: 'en',
+        eventDateFormat: 'DD/MM/YYYY',
+        useSaleAction: true
+        // optional, set this to false if you intend to call our sale endpoint manually
+    })
+
+    R.addEventListener('PgWidgetLoaded', function() {
+        const data = {
+            totalValue: 100,
+            numberOfTickets: 1,
+            eventTravelDateTime: '31/12/2024'
+        }
+
+        P._pgr('action', 'updateQuoteData', {...data})
+    })
+}(window, document, 'script', '_pgr', './widget.js'));
